e135aa378419ab92eb65ad5105f90d45f477f915
Clifford Steyn <clifford@softwxre.io>
2024-09-17 12:48:41 +0200

    Removed redundant TrainerNamePointers.

--- home/trainers.asm
+++ home/trainers.asm
@@ -353,7 +353,7 @@ PrintEndBattleText::
 	ldh [hLoadedROMBank], a
 	ld [MBC1RomBank], a
 	push hl
-	farcall SaveTrainerName
+	call SaveTrainerName
 	ld hl, TrainerEndBattleText
 	call PrintText
 	pop hl
@@ -363,6 +363,17 @@ PrintEndBattleText::
 	farcall FreezeEnemyTrainerSprite
 	jp WaitForSoundToFinish
 
+SaveTrainerName::
+	ld hl, wTrainerName
+	ld de, wcd6d
+.CopyCharacter
+	ld a, [hli]
+	ld [de], a
+	inc de
+	cp "@"
+	jr nz, .CopyCharacter
+	ret
+	
 GetSavedEndBattleTextPointer::
 	ld a, [wBattleResult]
 	and a

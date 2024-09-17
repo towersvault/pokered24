bd260ec72b10699b99e78fdfd846cdeda6cc97b9
Clifford Steyn <clifford@softwxre.io>
2024-09-17 13:10:25 +0200

    Removed Japanese Opening Quote and added Bold P

--- constants/charmap.asm
+++ constants/charmap.asm
@@ -68,7 +68,7 @@
 
 	charmap "<to>",      $70 ; narrow "to"
 
-	charmap "『",         $72 ; Japanese opening quote, unused
+	charmap "<BOLD_P>",	 $EA
 	charmap "<ID>",      $73
 	charmap "№",         $74
 
@@ -77,9 +77,6 @@
 	; needed for ShowPokedexDataInternal (see engine/menus/pokedex.asm)
 	charmap "′",         $60 ; gfx/pokedex/pokedex.png
 	charmap "″",         $61 ; gfx/pokedex/pokedex.png
-
-	; needed for StatusScreen (see engine/pokemon/status_screen.asm)
-	charmap "<BOLD_P>",  $72 ; gfx/font/P.1bpp
 
 	; needed for LoadTownMap_Fly (see engine/items/town_map.asm)
 	charmap "▲",         $ed ; gfx/town_map/up_arrow.1bpp
@@ -170,7 +167,6 @@
 	charmap ".",         $e8
 
 	charmap "ァ",         $e9 ; katakana small a, unused
-	charmap "ゥ",         $ea ; katakana small u, unused
 	charmap "ェ",         $eb ; katakana small e, unused
 
 	charmap "▷",         $ec

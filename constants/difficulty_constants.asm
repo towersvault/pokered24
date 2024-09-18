b5b665905e7fc73e30a2f823eb2903225fb4e788
Clifford Steyn <clifford@softwxre.io>
2024-09-18 15:17:31 +0200

    Changed level caps.

--- constants/difficulty_constants.asm
+++ constants/difficulty_constants.asm
@@ -11,12 +11,12 @@ const_value = $1
     const BADGE8
 
 ; Gym max levels
-DEF BADGECAP_BROCK      EQU 14 ; Onix's Level
+DEF BADGECAP_BROCK      EQU 12 ; Onix's Level
 DEF BADGECAP_MISTY      EQU 21 ; Starmie's Level
-DEF BADGECAP_LTSURGE    EQU 24 ; Raichu's Level
-DEF BADGECAP_ERIKA      EQU 29 ; Vileplume's Level
+DEF BADGECAP_LTSURGE    EQU 29 ; Raichu's Level
+DEF BADGECAP_ERIKA      EQU 35 ; Vileplume's Level
 DEF BADGECAP_KOGA       EQU 43 ; Weezing's Level
-DEF BADGECAP_SABRINA    EQU 43 ; Alakazam's Level
-DEF BADGECAP_BLAINE     EQU 47 ; Arcanine's Level
-DEF BADGECAP_GIOVANNI   EQU 50 ; Rhydon's Level
+DEF BADGECAP_SABRINA    EQU 50 ; Alakazam's Level
+DEF BADGECAP_BLAINE     EQU 53 ; Arcanine's Level
+DEF BADGECAP_GIOVANNI   EQU 55 ; Rhydon's Level
 DEF BADGECAP_BLUE       EQU 65 ; Venusaur/Charizard/Blastoise's Level
